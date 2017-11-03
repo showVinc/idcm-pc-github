ce9ead5a77a3040e19e8afc0ce3bb722fc70b8d3
fanhh <1825923694@qq.com>
2017-11-03 18:29:19 +0800

    个人中心手机绑定-修改资料完成

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import Index from '@/pages/Index'
 import SettingIndex from '@/pages/setting/set'    //  安全设置
 import SettingLog from '@/pages/setting/log'    //  安全记录
 import SettingCertify from '@/pages/setting/certify'    //  实名认证
+import SetMobile from '@/pages/setting/mobile'    //  绑定邮箱
+import SetFundpassword from '@/pages/setting/fundpassword'    //  绑定邮箱
 
 Vue.use(Router)
 
@@ -20,7 +22,19 @@ export default new Router({
       pos: 1,            //  在安全设置菜单中的位置
       path: '/setting/index',
       name: '安全设置',
-      component: SettingIndex
+      component: SettingIndex,
+      children: [
+        {
+          path: '/setting/index/mobile',
+          name: '邮箱绑定',
+          component: SetMobile,
+        },
+        {
+          path: '/setting/index/fundpassword',
+          name: '资金密码',
+          component: SetFundpassword
+        }
+      ]
     },
     {
       type: 'setting',
